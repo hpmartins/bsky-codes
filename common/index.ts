38ccb724da6e1a2f86d1ee6656ca50b095e0581e
hpmartins <hpmartins@gmail.com>
2023-11-06 23:53:21 -0800

    fixing lexicon refs

--- common/index.ts
+++ common/index.ts
@@ -10,9 +10,9 @@ import {
 import Bottleneck from 'bottleneck';
 import { Block, Post, Profile, Interaction, SyncProfile, SyncState } from '../common/db';
 import { AtprotoData } from '@atproto/identity';
-import { ProfileViewDetailed } from "../lexicon/types/app/bsky/actor/defs";
-import { OutputSchema as ListReposSchema } from "../lexicon/types/com/atproto/sync/listRepos";
-import { OutputSchema as ListRecordsSchema } from "../lexicon/types/com/atproto/repo/listRecords";
+import { ProfileViewDetailed } from "./lexicon/types/app/bsky/actor/defs";
+import { OutputSchema as ListReposSchema } from "./lexicon/types/com/atproto/sync/listRepos";
+import { OutputSchema as ListRecordsSchema } from "./lexicon/types/com/atproto/repo/listRecords";
 
 export * from "./db/index";
 export * from "./db/schema";
