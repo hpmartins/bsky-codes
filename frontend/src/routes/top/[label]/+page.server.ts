14f2ae99bc9cf7b5d7136f0d4d9532a72c8bfcaf
hpmartins <hpmartins@gmail.com>
2023-11-07 20:48:41 -0800

    disabling top stuff

--- frontend/src/routes/top/[label]/+page.server.ts
+++ frontend/src/routes/top/[label]/+page.server.ts
@@ -1,21 +1,21 @@
-import { getTopBlocked, getTopPosters } from "$lib/server/utils";
 import { error } from "@sveltejs/kit";
 import type { PageServerLoad } from "./$types";
 
 export const load: PageServerLoad = async ({ params } ) => {
-    if (params.label === 'blocks') {
-        const list = await getTopBlocked()
-        return {
-            type: params.label,
-            list: list.map((x, idx) => ({idx: idx+1, ...x}))
-        }
-    } else if (params.label === 'posters') {
-        const list = await getTopPosters()
-        return {
-            type: params.label,
-            list: list.map((x, idx) => ({idx: idx+1, ...x}))
-        } 
-    } else {
-        throw error(404, 'not found');
-    }
+    // if (params.label === 'blocks') {
+    //     const list = await getTopBlocked()
+    //     return {
+    //         type: params.label,
+    //         list: list.map((x, idx) => ({idx: idx+1, ...x}))
+    //     }
+    // } else if (params.label === 'posters') {
+    //     const list = await getTopPosters()
+    //     return {
+    //         type: params.label,
+    //         list: list.map((x, idx) => ({idx: idx+1, ...x}))
+    //     } 
+    // } else {
+    //     throw error(404, 'not found');
+    // }
+    return {}
 }
--- frontend/src/routes/top/[label]/+page.svelte
+++ frontend/src/routes/top/[label]/+page.svelte
@@ -1,6 +1,6 @@
 <script lang="ts">
     import type { PageServerData } from "./$types";
-    import TopBlocksTable from "./TopBlocksTable.svelte";
+    import TopBlockedTable from "./TopBlockedTable.svelte";
     import TopPostersTable from "./TopPostersTable.svelte";
 
     export let data: PageServerData
@@ -8,10 +8,10 @@
 
 <div class="row justify-content-center my-3">
     <div class="col-sm-12 col-md-6 my-3">
-        {#if data.type === 'blocks'}
-        <TopBlocksTable data={data.list} perPage={20} />
+        <!-- {#if data.type === 'blocks'}
+        <TopBlockedTable data={data.list} perPage={20} />
         {:else if data.type === 'posters'}
         <TopPostersTable data={data.list} perPage={20} />
-        {/if}
+        {/if} -->
     </div>
 </div>
