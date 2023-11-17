9d2823ef772639b9aff782acbe7f556bb62fad95
hpmartins <hpmartins@gmail.com>
2023-11-16 18:29:35 -0800

    added cards and stats

--- frontend/src/routes/+page.server.ts
+++ frontend/src/routes/+page.server.ts
@@ -1,6 +1,21 @@
+import { Block, Interaction, Post, Profile } from '@common/db';
 import type { PageServerLoad } from './$types';
+import dayjs from 'dayjs';
+
+
+const formatNumber = (s: string) => s.replace(/\B(?<!\.\d*)(?=(\d{3})+(?!\d))/g, ",")
 
 export const load: PageServerLoad = async () => {
+    // const query = await TopPostersByLang.aggregate()
+    // .match({
+    //     _id: { $gte: dayjs().subtract(3, 'day').startOf('day').toDate() }
+    // })
+    // .project({ _id: 0, items: 1 })
+    // .unwind('$items')
+    // .project({ lang: "$items._id" })
+
+    // const langs = [...new Set(query.map(x => x.lang))]
+    // console.log(langs)
 //   const query = await DataHistogram.find(
 //     { _id: { $gte: dayjs('2022-10-01').toDate() }}
 //   )
@@ -8,4 +23,18 @@ export const load: PageServerLoad = async () => {
 //   return {
 //     histogram: query.map(x => x.toObject()),
 //   }
+
+    const aggBlocks = await Block.aggregate( [ { $collStats: { count: {} } } ] )
+    const aggPosts = await Post.aggregate( [ { $collStats: { count: {} } } ] )
+    const aggInteractions = await Interaction.aggregate( [ { $collStats: { count: {} } } ] )
+    const aggProfiles = await Profile.aggregate( [ { $collStats: { count: {} } } ] )
+
+    return {
+      count: {
+        blocks: formatNumber(aggBlocks[0].count.toString()),
+        posts: formatNumber(aggPosts[0].count.toString()),
+        interactions: formatNumber(aggInteractions[0].count.toString()),
+        profiles: formatNumber(aggProfiles[0].count.toString()),
+    }
+  }
 };
--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -1,6 +1,68 @@
-<div class="md:container md:mx-auto p-12 space-y-8">
+<script lang="ts">
+  import type { PageServerData } from './$types';
+
+  export let data: PageServerData;
+</script>
+
+<div class="md:container md:mx-auto center p-12 space-y-8">
   <p class="text-xl font-bold">Welcome to Wolfgang!</p>
   <p>
-    Information about <a href="https://bsky.app/">Bluesky</a> accounts and their interactions
+    Information about <a class="link" href="https://bsky.app/">Bluesky</a> accounts. Not all data is indexed yet.
   </p>
+
+  <div class="flex justify-center">
+  <div class="stats stats-vertical md:stats-horizontal shadow">
+    <div class="stat">
+      <div class="stat-title">Profiles</div>
+      <div class="stat-value">{data.count.profiles}</div>
+    </div>
+    <div class="stat">
+      <div class="stat-title">Posts</div>
+      <div class="stat-value">{data.count.posts}</div>
+    </div>
+    <div class="stat">
+      <div class="stat-title">Blocks</div>
+      <div class="stat-value">{data.count.blocks}</div>
+    </div>
+    <div class="stat">
+      <div class="stat-title">Interactions</div>
+      <div class="stat-value">{data.count.interactions}</div>
+    </div>
+  </div>
+</div>
+
+  <div class="flex flex-row flex-grow flex-wrap justify-center gap-4">
+    <div class="card w-full md:w-80 bg-primary text-secondary">
+      <a href="/interactions">
+        <div class="card-body p-3">
+          <h2 class="card-title">Interactions</h2>
+          <p>Find your interactions by week and create an image with the top profiles</p>
+        </div>
+      </a>
+    </div>
+    <div class="card w-full md:w-80 bg-primary text-secondary">
+      <a href="/blocks">
+        <div class="card-body p-3">
+          <h2 class="card-title">Blocks</h2>
+          <p>Search who anyone blocks or is blocked by</p>
+        </div>
+      </a>
+    </div>
+    <div class="card w-full md:w-80 bg-primary text-secondary">
+      <a href="/top/blocked">
+        <div class="card-body p-3">
+          <h2 class="card-title">Top blocked</h2>
+          <p>Most blocked accounts in the last 72 hours</p>
+        </div>
+      </a>
+    </div>
+    <div class="card w-full md:w-80 bg-primary text-secondary">
+      <a href="/top/posters">
+        <div class="card-body p-3">
+          <h2 class="card-title">Top posters</h2>
+          <p>Who most posted in the last 72 hours</p>
+        </div>
+      </a>
+    </div>
+  </div>
 </div>
