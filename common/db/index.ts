86f336e1265953ee4727854d13a82322d13f442d
hpmartins <hpmartins@gmail.com>
2023-11-17 11:18:51 -0800

    removed msg

--- common/db/index.ts
+++ common/db/index.ts
@@ -9,18 +9,15 @@ const mongoConnection = {
 
 export const connectDb = async (uri?: string) => {
     if (mongoConnection.isConnected) {
-        console.log('(mongodb) already connected');
         return;
     }
     if (mongoose.connections.length > 0) {
         mongoConnection.isConnected = Boolean(mongoose.connections[0].readyState);
         if (mongoConnection.isConnected) {
-            console.log('(mongodb) already connected');
             return;
         }
         await mongoose.disconnect();
     }
     await mongoose.connect(uri ?? process.env.MONGODB_URI ?? 'mongodb://localhost');
-    console.log('(mongodb) connected');
     mongoConnection.isConnected = true;
 };
