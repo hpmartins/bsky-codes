2300e51ea1f5cad155bbea15b23829162dd1611e
hpmartins <hpmartins@gmail.com>
2023-12-01 11:59:39 -0800

    added outofcontext to do_not_include_list

--- common/index.ts
+++ common/index.ts
@@ -23,6 +23,7 @@ export const DO_NOT_INCLUDE_THESE = [
   'did:plc:db645kt5coo7teuoxdjhq34x', // @blueskybaddies
   'did:plc:y4rd5hesgwwbkblvkkidfs73', // @wolfgang
   'did:plc:iw47x7htlvpkbbizqn2sgnks', // @whatsmid
+  'did:plc:fxj7klwmhjh75ow5bmlyp5g2', // @outofcontext
 ];
 
 export const getDateTime = (date?: number | Date) => {
