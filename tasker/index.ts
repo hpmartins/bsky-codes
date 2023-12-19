0af68359a93f5e6b2630e344770b344df1edc5e5
hpmartins <hpmartins@gmail.com>
2023-12-19 03:57:27 +0000

    word cloud

--- tasker/index.ts
+++ tasker/index.ts
@@ -3,15 +3,31 @@ import 'module-alias/register';
 import express from 'express';
 import Bottleneck from 'bottleneck';
 import { Profile, SyncProfile, connectDb } from '../common/db';
-import AtpAgent, { BskyAgent } from '@atproto/api';
+import AtpAgent, { AppBskyFeedPost, BskyAgent } from '@atproto/api';
 import { DidResolver } from '@atproto/identity';
 import { getCreationTimestamp, listRepos, maybeBoolean, maybeInt, maybeStr } from '../common';
-import { syncBlockRecords, syncOneProfile, syncPostRecords, syncWaitingProfiles } from './tasks/sync';
-import { storeBlocksHistogram, storeFollowsHistogram, storeLikesHistogram, storePostsByLang, storePostsHistogram, storeProfilesHistogram, storeRepostsHistogram, storeTopBlocked, storeTopPosters } from './tasks/stats';
+import {
+    syncBlockRecords,
+    syncOneProfile,
+    syncPostRecords,
+    syncWaitingProfiles
+} from './tasks/sync';
+import {
+    storeBlocksHistogram,
+    storeFollowsHistogram,
+    storeLikesHistogram,
+    storePostsByLang,
+    storePostsHistogram,
+    storeProfilesHistogram,
+    storeRepostsHistogram,
+    storeTopBlocked,
+    storeTopPosters
+} from './tasks/stats';
 import { updateLickablePeople, updateLickablePosts } from './tasks/wolfgang';
-import { Manager } from "socket.io-client";
+import { Manager } from 'socket.io-client';
 import redis, { createClient } from 'redis';
 import dayjs from 'dayjs';
+import { createLatestEmojiCloud, createLatestWordCloud, postClouds } from './tasks/wordcloud';
 
 export const MINUTE = 60;
 export const HOUR = MINUTE * 60;
@@ -19,196 +35,221 @@ export const DAY = HOUR * 24;
 export const WEEK = DAY * 7;
 
 type AppConfig = {
-  devel: boolean;
-  bskyDid: string;
-  bskyPwd: string;
-  redisHost: string;
-  redisPort: number;
-}
+    devel: boolean;
+    bskyDid: string;
+    bskyPwd: string;
+    bskyDid2: string;
+    bskyPwd2: string;
+    redisHost: string;
+    redisPort: number;
+};
 
 export type AppContext = {
-  cfg: AppConfig;
-  app: express.Express;
-  api: AtpAgent;
-  agent: BskyAgent;
-  didres: DidResolver;
-  limiter: Bottleneck;
-  cache: redis.RedisClientType<any, any, any>;
-  log: (text: string) => void;
+    cfg: AppConfig;
+    app: express.Express;
+    api: AtpAgent;
+    agent: BskyAgent;
+    agent2: BskyAgent;
+    didres: DidResolver;
+    limiter: Bottleneck;
+    cache: redis.RedisClientType<any, any, any>;
+    log: (text: string) => void;
 };
 
 function scheduleTasks(ctx: AppContext) {
-  cron.schedule('*/2 * * * *', async () => {
-    await syncWaitingProfiles(ctx);
-  });
+    cron.schedule('*/2 * * * *', async () => {
+        await syncWaitingProfiles(ctx);
+    });
 
-  cron.schedule('0 */6 * * *', async () => {
-    await storeTopBlocked();
-  });
+    cron.schedule('0 */6 * * *', async () => {
+        await storeTopBlocked();
+    });
 
-  cron.schedule('0 */6 * * *', async () => {
-    await storeTopPosters();
-  });
+    cron.schedule('0 */6 * * *', async () => {
+        await storeTopPosters();
+    });
 
-  cron.schedule('*/5 * * * *', async () => {
-    const lickablePeople = await updateLickablePeople(ctx)
-    await updateLickablePosts(ctx, lickablePeople)
-  })
+    cron.schedule('*/5 * * * *', async () => {
+        const lickablePeople = await updateLickablePeople(ctx);
+        await updateLickablePosts(ctx, lickablePeople);
+    });
 
-  cron.schedule('5 */2 * * *', async () => {
-    const after = dayjs().subtract(2, 'day').startOf('day').toDate();
-    await storeBlocksHistogram(after);
-    await storeFollowsHistogram(after);
-    await storeLikesHistogram(after);
-    await storeProfilesHistogram(after);
-    await storeRepostsHistogram(after);
-    await storePostsHistogram(after);
-  })
+    cron.schedule('5 */2 * * *', async () => {
+        const after = dayjs().subtract(2, 'day').startOf('day').toDate();
+        await storeBlocksHistogram(after);
+        await storeFollowsHistogram(after);
+        await storeLikesHistogram(after);
+        await storeProfilesHistogram(after);
+        await storeRepostsHistogram(after);
+        await storePostsHistogram(after);
+    });
 
-  cron.schedule('10 */3 * * *', async () => {
-    const after = dayjs().subtract(6, 'hour').startOf('hour').toDate();
-    await storePostsByLang(after);
-  })
+    cron.schedule('10 */3 * * *', async () => {
+        const after = dayjs().subtract(6, 'hour').startOf('hour').toDate();
+        await storePostsByLang(after);
+    });
+
+    cron.schedule(
+        '0 2-6/2 * * *', // every 2 hours from 2-6 am
+        async () => {
+            await postClouds(ctx, 135);
+        },
+        { timezone: 'America/Sao_Paulo' }
+    );
+
+    cron.schedule(
+        '0 7-23,0 * * *', // every hour from 7-23 and 0
+        async () => {
+            await postClouds(ctx, 75);
+        },
+        { timezone: 'America/Sao_Paulo' }
+    );
 }
 
 async function updateIndexedAt(ctx: AppContext) {
-  let repoCursor: string | undefined;
-  do {
-    const response = await ctx.limiter.schedule(async () =>
-      listRepos({
-        limit: 3,
-        cursor: repoCursor
-      })
-    );
+    let repoCursor: string | undefined;
+    do {
+        const response = await ctx.limiter.schedule(async () =>
+            listRepos({
+                limit: 3,
+                cursor: repoCursor
+            })
+        );
 
-    if (response && response.repos) {
-        for (const repo of response.repos) {
-            const ts_data = await ctx.limiter.schedule(async () =>
-                getCreationTimestamp(repo.did)
-            );
-            if (!ts_data) continue;
-            const { handle, indexedAt } = ts_data;
-            await Profile.updateOne(
-                { _id: repo.did },
-                {
-                    handle: handle,
-                    indexedAt: indexedAt
-                },
-                { upsert: true }
-            );
+        if (response && response.repos) {
+            for (const repo of response.repos) {
+                const ts_data = await ctx.limiter.schedule(async () =>
+                    getCreationTimestamp(repo.did)
+                );
+                if (!ts_data) continue;
+                const { handle, indexedAt } = ts_data;
+                await Profile.updateOne(
+                    { _id: repo.did },
+                    {
+                        handle: handle,
+                        indexedAt: indexedAt
+                    },
+                    { upsert: true }
+                );
+            }
         }
-    }
-    repoCursor = response?.cursor ?? undefined;
-  } while (!!repoCursor && repoCursor.length > 0);
+        repoCursor = response?.cursor ?? undefined;
+    } while (!!repoCursor && repoCursor.length > 0);
 }
 
 async function run() {
-  await connectDb();
+    await connectDb();
 
-  const listenerHost = maybeStr(process.env.LISTENER_HOST) ?? 'localhost'
-  const listenerPort = maybeInt(process.env.LISTENER_PORT) ?? 6002
-  const manager = new Manager(`ws://${listenerHost}:${listenerPort}`);
-  const socket = manager.socket('/');
-  socket.on("connect", () => {
-    log('connected to listener')
-  })
-  socket.io.on("reconnect", (attempt: number) => {
-    log(`reconnected to listener [${attempt}]`)
-  })
-  socket.io.on("reconnect_attempt", (attempt: number) => {
-    log(`reconnecting to listener... [${attempt}]`)
-  })
-
-  const cfg: AppConfig = {
-    devel: maybeBoolean(process.env.DEVEL) ?? true,
-    bskyDid: maybeStr(process.env.WOLFGANG_BSKY_DID) ?? '',
-    bskyPwd: maybeStr(process.env.WOLFGANG_BSKY_PASSWORD) ?? '',
-    redisHost: maybeStr(process.env.REDIS_HOST) ?? 'localhost',
-    redisPort: maybeInt(process.env.REDIS_PORT) ?? 6379,
-  }
-
-  const app = express();
-  const didres = new DidResolver({});
-  const limiter = new Bottleneck({
-    maxConcurrent: 1,
-    minTime: (5 * 60000) / 2000
-  });
-  const log = (text: string) => {
-    console.log(`[${new Date().toLocaleTimeString()}] [tasker] ${text}`);
-  };
-
-  const agent = new BskyAgent({ service: 'https://bsky.social/' })
-  await agent.login({ identifier: cfg.bskyDid, password: cfg.bskyPwd });
-
-  const api = new AtpAgent({ service: 'https://bsky.social/' })
-
-  const cache = await createClient({
-    url: process.env.REDIS_URI,
-  })
-    .on('error', err => log(`redis error: ${err}`))
-    .on('connect', () => log('connected to redis'))
-    .connect()
-
-  limiter.on('failed', async (error: Error, jobInfo) => {
-    if (error.message.includes('Could not find repo')) {
-      return;
-    }
-    ctx.log(error.message)
-    ctx.log('Retrying in 10s...');
-    return 10000;
-  });
-
-  const ctx: AppContext = {
-    cfg,
-    app,
-    api,
-    agent,
-    didres,
-    limiter,
-    cache,
-    log,
-  };
-
-  if (!cfg.devel) {
-    log('starting tasks');
-    scheduleTasks(ctx);
-  }
-
-  // Sync stuff
-  const syncCheck = maybeBoolean(process.env.TASKER_ENABLE_SYNC) ?? false;
-  if (syncCheck) {
-    socket.on("data", async (data: { repo: string }): Promise<void> => {
-      const { repo } = data;
-      
-      if (await ctx.cache.hExists('syncPosts', repo)) {
-        const totalPosts = await syncPostRecords(ctx, repo)
-        log(`[sync] ${repo}: posts:${String(totalPosts).padStart(4, ' ')}`)
-        await ctx.cache.hSet('syncPosts', repo, 1);
-      }
-  
-      if (await ctx.cache.hExists('syncBlocks', repo)) {
-        const totalBlocks = await syncBlockRecords(ctx, repo)
-        log(`[sync] ${repo}: blocks:${String(totalBlocks).padStart(4, ' ')}`)
-        await ctx.cache.hSet('syncBlocks', repo, 1);
-      }
+    const listenerHost = maybeStr(process.env.LISTENER_HOST) ?? 'localhost';
+    const listenerPort = maybeInt(process.env.LISTENER_PORT) ?? 6002;
+    const manager = new Manager(`ws://${listenerHost}:${listenerPort}`);
+    const socket = manager.socket('/');
+    socket.on('connect', () => {
+        log('connected to listener');
     });
-  }
+    socket.io.on('reconnect', (attempt: number) => {
+        log(`reconnected to listener [${attempt}]`);
+    });
+    socket.io.on('reconnect_attempt', (attempt: number) => {
+        log(`reconnecting to listener... [${attempt}]`);
+    });
 
-  app.get('/update/:did', async (req, res) => {
-    const doc = await didres.resolveAtprotoData(req.params.did);
-    if (!doc) return res.send('could not find did');
-    try {
-      await syncOneProfile(ctx, doc);
-    } catch (e) {
-      return res.send(e);
+    const cfg: AppConfig = {
+        devel: maybeBoolean(process.env.DEVEL) ?? true,
+        bskyDid: maybeStr(process.env.WOLFGANG_BSKY_DID) ?? '',
+        bskyPwd: maybeStr(process.env.WOLFGANG_BSKY_PASSWORD) ?? '',
+        bskyDid2: maybeStr(process.env.BOLBI_BSKY_DID) ?? '',
+        bskyPwd2: maybeStr(process.env.BOLBI_BSKY_PASSWORD) ?? '',
+        redisHost: maybeStr(process.env.REDIS_HOST) ?? 'localhost',
+        redisPort: maybeInt(process.env.REDIS_PORT) ?? 6379
+    };
+
+    const app = express();
+    const didres = new DidResolver({});
+    const limiter = new Bottleneck({
+        maxConcurrent: 1,
+        minTime: (5 * 60000) / 2000
+    });
+    const log = (text: string) => {
+        console.log(`[${new Date().toLocaleTimeString()}] [tasker] ${text}`);
+    };
+
+    const agent = new BskyAgent({ service: 'https://bsky.social/' });
+    await agent.login({ identifier: cfg.bskyDid, password: cfg.bskyPwd });
+
+    const agent2 = new BskyAgent({ service: 'https://bsky.social/' });
+    await agent2.login({ identifier: cfg.bskyDid2, password: cfg.bskyPwd2 });
+
+    const api = new AtpAgent({ service: 'https://bsky.social/' });
+
+    const cache = await createClient({
+        url: process.env.REDIS_URI
+    })
+        .on('error', (err) => log(`redis error: ${err}`))
+        .on('connect', () => log('connected to redis'))
+        .connect();
+
+    limiter.on('failed', async (error: Error, jobInfo) => {
+        if (error.message.includes('Could not find repo')) {
+            return;
+        }
+        ctx.log(error.message);
+        ctx.log('Retrying in 10s...');
+        return 10000;
+    });
+
+    const ctx: AppContext = {
+        cfg,
+        app,
+        api,
+        agent,
+        agent2,
+        didres,
+        limiter,
+        cache,
+        log
+    };
+
+    if (!cfg.devel) {
+        log('starting tasks');
+        scheduleTasks(ctx);
     }
-    return res.send('done!');
-  });
 
-  const port = maybeInt(process.env.TASKER_PORT) ?? 6001;
-  app.listen(port, () => {
-    ctx.log(`listening @ :${port}`);
-  });
+    // Sync stuff
+    const syncCheck = maybeBoolean(process.env.TASKER_ENABLE_SYNC) ?? false;
+    if (syncCheck) {
+        socket.on('data', async (data: { repo: string }): Promise<void> => {
+            const { repo } = data;
+
+            if (await ctx.cache.hExists('syncPosts', repo)) {
+                const totalPosts = await syncPostRecords(ctx, repo);
+                log(`[sync] ${repo}: posts:${String(totalPosts).padStart(4, ' ')}`);
+                await ctx.cache.hSet('syncPosts', repo, 1);
+            }
+
+            if (await ctx.cache.hExists('syncBlocks', repo)) {
+                const totalBlocks = await syncBlockRecords(ctx, repo);
+                log(`[sync] ${repo}: blocks:${String(totalBlocks).padStart(4, ' ')}`);
+                await ctx.cache.hSet('syncBlocks', repo, 1);
+            }
+        });
+    }
+
+    app.get('/update/:did', async (req, res) => {
+        const doc = await didres.resolveAtprotoData(req.params.did);
+        if (!doc) return res.send('could not find did');
+        try {
+            await syncOneProfile(ctx, doc);
+        } catch (e) {
+            return res.send(e);
+        }
+        return res.send('done!');
+    });
+
+    const port = maybeInt(process.env.TASKER_PORT) ?? 6001;
+    app.listen(port, () => {
+        ctx.log(`listening @ :${port}`);
+    });
 }
 
 run();
---
+++ tasker/tasks/wordcloud.ts
@@ -0,0 +1,267 @@
+import dayjs from 'dayjs';
+import { Post } from '../../common/db';
+
+import canvas from 'canvas';
+import cloud, { Word } from 'd3-cloud';
+import { EXCLUDE_WORDS } from '../const';
+import sharp from 'sharp';
+import D3Node from 'd3-node';
+import chroma from 'chroma-js';
+import { AppContext } from 'index';
+import { AppBskyEmbedImages, AppBskyFeedPost } from '@atproto/api';
+
+import utc from 'dayjs/plugin/utc';
+import timezone from 'dayjs/plugin/timezone';
+
+dayjs.extend(utc);
+dayjs.extend(timezone);
+
+const TIMEZONE = 'America/Sao_Paulo';
+
+export const postClouds = async (ctx: AppContext, minutes: number) => {
+    const clouds = [await createLatestWordCloud(minutes), await createLatestEmojiCloud(minutes)];
+
+    const uploadedImages = await Promise.all(
+        clouds.map(async (cld) => {
+            const uploaded = await ctx.agent2.uploadBlob(cld.image, { encoding: 'image/png' });
+            if (!uploaded.success) throw new Error('Failed to upload image');
+
+            return {
+                image: uploaded.data.blob,
+                alt: cld.alt
+            } satisfies AppBskyEmbedImages.Image;
+        })
+    );
+
+    const embed =
+        uploadedImages.length > 0
+            ? {
+                  $type: 'app.bsky.embed.images',
+                  images: uploadedImages
+              }
+            : undefined;
+
+    if (embed) {
+        const timeFrom = dayjs().subtract(minutes, 'minutes').tz(TIMEZONE).format('HH:mm');
+        const timeTo = dayjs().tz(TIMEZONE).format('HH:mm');
+
+        const shortWordList = embed.images[0].alt.split('\n').slice(0, 3).join('\n');
+        const shortEmojiList = embed.images[1].alt.split('\n').slice(0, 3).join('\n');
+        const postRecord = {
+            $type: 'app.bsky.feed.post',
+            text: `Nuvem de palavras e emojis de posts em português (${timeFrom} até ${timeTo})\n\n${shortWordList}\n\n${shortEmojiList}`,
+            createdAt: new Date().toISOString(),
+            embed: embed
+        };
+
+        if (
+            AppBskyFeedPost.isRecord(postRecord) &&
+            AppBskyFeedPost.validateRecord(postRecord).success
+        ) {
+            await ctx.agent2.post(postRecord);
+        }
+    }
+};
+
+export const createLatestWordCloud = async (minutes: number) => {
+    const words = await getLatestWordCloud(minutes);
+
+    const normalizedWords = words
+        .filter((x) => x._id.length > 1)
+        .filter((x) => !/\p{Emoji}/u.test(x._id))
+        .map((x) => ({
+            text: x._id,
+            size: x.count
+        }));
+
+    const colorScale = chroma
+        .scale(['#006BB6', '#FDB927'])
+        .domain([words[words.length - 1].count, words[0].count])
+        .mode('lch');
+
+    const d3n = new D3Node();
+
+    cloud()
+        .size([900, 900])
+        .words(normalizedWords)
+        .rotate(() => ~~(Math.random() * 2) * 45)
+        .canvas(() => canvas.createCanvas(1, 1))
+        .random(() => 0.5)
+        .fontSize((d) => (d.size ? (100 * d.size) / words[0].count : 0))
+        .fontStyle((d) => colorScale(d.size).hex())
+        .padding(4)
+        .on('end', draw)
+        .start();
+
+    async function draw(words: { [key: string]: any }[], bounds: { [key: string]: number }[]) {
+        const { x: width, y: height } = bounds[1];
+
+        const svg = d3n.createSVG(width, height);
+        svg.append('rect').attr('width', '100%').attr('height', '100%').attr('fill', '#343434');
+
+        svg.append('g')
+            .attr('transform', 'translate(' + width / 2 + ',' + height / 2 + ')')
+            .selectAll('text')
+            .data(words)
+            .enter()
+            .append('text')
+            .style('font-size', (d: Word) => d.size + 'px')
+            .style('fill', (d: Word) => d.style)
+            .style('font-family', 'Impact')
+            .attr('text-anchor', 'middle')
+            .attr('transform', (d: Word) => {
+                return 'translate(' + [d.x, d.y] + ')rotate(' + d.rotate + ')';
+            })
+            .text((d: Word) => d.text);
+    }
+
+    return {
+        alt: normalizedWords
+            .slice(0, 40)
+            .map((word, idx) => `${idx + 1}. ${word.text} (${word.size})`)
+            .join('\n'),
+        image: await sharp(Buffer.from(d3n.svgString())).png().toBuffer()
+    };
+};
+
+export const createLatestEmojiCloud = async (minutes: number) => {
+    const words = await getLatestEmojiCloud(minutes);
+
+    const normalizedWords = words
+        .filter((x) => !['⬛', '🟩', '🟨', '🟥', '🟪', '🟦', '⬜'].includes(x._id))
+        .map((x) => ({
+            text: x._id,
+            size: x.count
+        }));
+
+    const cv = canvas.createCanvas(1, 1);
+
+    cloud()
+        .size([900, 900])
+        .words(normalizedWords)
+        .rotate(0)
+        .canvas(() => canvas.createCanvas(1, 1))
+        .random(() => Math.random())
+        .font('Noto Color Emoji')
+        .fontSize((d) => (d.size ? 100 * Math.pow(d.size / words[0].count, 0.25) : 0))
+        .padding(15)
+        .spiral('rectangular')
+        .on('end', draw)
+        .start();
+
+    async function draw(words: { [key: string]: any }[], bounds: { [key: string]: number }[]) {
+        const { x: width, y: height } = bounds[1];
+
+        cv.width = width;
+        cv.height = height;
+        const ctx = cv.getContext('2d');
+
+        ctx.fillStyle = '#343434';
+        ctx.fillRect(0, 0, width, height);
+
+        ctx.textAlign = 'center';
+        ctx.textBaseline = 'middle';
+        ctx.textDrawingMode = 'glyph';
+
+        for (let word of words) {
+            ctx.font = `${word.size}px ${word.font}`;
+            ctx.fillText(word.text, width / 2 + word.x, height / 2 + word.y);
+        }
+    }
+
+    return {
+        alt: normalizedWords
+            .slice(0, 40)
+            .map((word, idx) => `${idx + 1}. ${word.text} (${word.size})`)
+            .join('\n'),
+        image: cv.toBuffer()
+    };
+};
+
+export const getLatestEmojiCloud = async (
+    minutes: number
+): Promise<
+    {
+        _id: string;
+        count: number;
+    }[]
+> => {
+    const query = await Post.aggregate()
+        .match({
+            langs: 'pt',
+            createdAt: {
+                $gte: dayjs().subtract(minutes, 'minutes').toDate()
+            },
+            textLength: {
+                $gt: 0
+            },
+            text: {
+                $regex: /\p{Extended_Pictographic}/,
+                $options: 'u'
+            },
+            deleted: false,
+        })
+        .project({
+            _id: 0,
+            emojis: {
+                $regexFindAll: {
+                    input: '$text',
+                    regex: /\p{Extended_Pictographic}/u
+                }
+            }
+        })
+        .unwind('$emojis')
+        .project({
+            emojis: '$emojis.match'
+        })
+        .sortByCount('$emojis')
+        .limit(40);
+
+    return query;
+};
+
+export const getLatestWordCloud = async (
+    minutes: number
+): Promise<
+    {
+        _id: string;
+        count: number;
+    }[]
+> => {
+    const query = await Post.aggregate()
+        .match({
+            langs: 'pt',
+            createdAt: {
+                $gte: dayjs().subtract(minutes, 'minutes').toDate()
+            },
+            textLength: {
+                $gt: 0
+            },
+            deleted: false,
+        })
+        .project({
+            _id: 0,
+            words: {
+                $map: {
+                    input: { $split: ['$text', ' '] },
+                    as: 'str',
+                    in: {
+                        $trim: {
+                            input: { $toLower: ['$$str'] },
+                            chars: ' ,|(){}-<>.;\n'
+                        }
+                    }
+                }
+            }
+        })
+        .unwind('$words')
+        .match({
+            words: {
+                $nin: EXCLUDE_WORDS
+            }
+        })
+        .sortByCount('$words')
+        .limit(100);
+
+    return query.filter((x) => x._id.length > 1).filter((x) => !/\p{Emoji}/u.test(x._id));
+};
