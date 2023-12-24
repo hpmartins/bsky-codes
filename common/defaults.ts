eb7c6db3318fc2111262175594879cc4e678ebc6
hpmartins <hpmartins@gmail.com>
2023-12-24 23:22:52 +0000

    added luna and bolbi to bot list

--- common/defaults.ts
+++ common/defaults.ts
@@ -2,6 +2,8 @@ export const DO_NOT_INCLUDE_THESE = [
     'did:plc:xxno7p4xtpkxtn4ok6prtlcb', // @lovefairy.nl
     'did:plc:db645kt5coo7teuoxdjhq34x', // @blueskybaddies
     'did:plc:y4rd5hesgwwbkblvkkidfs73', // @wolfgang
+    'did:plc:fatjkn3ztc7tyhgar3rfjahb', // @luna
+    'did:plc:6agw63p4t7wh7y5qta4iyqfb', // @bolbi
     'did:plc:iw47x7htlvpkbbizqn2sgnks', // @whatsmid
     'did:plc:fxj7klwmhjh75ow5bmlyp5g2', // @outofcontext
 ];
