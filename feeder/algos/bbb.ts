742c1966f014a21047d04fea10d1b83e6f409f2f
hpmartins <hpmartins@gmail.com>
2024-01-08 22:08:32 +0000

    bbb feed

---
+++ feeder/algos/bbb.ts
@@ -0,0 +1,90 @@
+import { InvalidRequestError } from '@atproto/xrpc-server';
+import { QueryParams } from '../../common/lexicon/types/app/bsky/feed/getFeedSkeleton';
+import { PostsPt } from '../../common/db';
+import dayjs from 'dayjs';
+
+// max 15 chars
+export const shortname = 'bbb';
+
+export const handler = async (params: QueryParams) => {
+    let qb = PostsPt.aggregate()
+        .match({
+            createdAt: {
+                $gte: dayjs().subtract(36, 'h').toDate()
+            }
+        })
+        .addFields({
+            n: { $size: '$facets' }
+        })
+        .match({
+            n: { $gt: 0 }
+        })
+        .addFields({
+            tags: {
+                $reduce: {
+                    input: '$facets.features.tag',
+                    initialValue: [],
+                    in: { $concatArrays: ['$$value', '$$this'] }
+                }
+            }
+        })
+        .addFields({
+            n: { $size: '$tags' }
+        })
+        .match({
+            n: { $gt: 0 }
+        })
+        .addFields({
+            tags: {
+                $map: {
+                    input: '$tags',
+                    as: 'tags',
+                    in: { $toLower: '$$tags' }
+                }
+            }
+        })
+        .project({
+            _id: 1,
+            cid: 1,
+            createdAt: 1,
+            tags: 1
+        })
+        .match({
+            tags: {
+                $in: ['bbb', 'bbb24', 'bigbrother', 'bigbrotherbrasil', 'bigbrotherbrasil24']
+            }
+        })
+        .sort({
+            createdAt: 'desc',
+            cid: 'desc'
+        })
+        .limit(params.limit);
+
+    if (params.cursor) {
+        const [createdAt, cid] = params.cursor.split('::');
+        if (!createdAt || !cid) {
+            throw new InvalidRequestError('malformed cursor');
+        }
+        const timeStr = new Date(parseInt(createdAt, 10)).toISOString();
+        qb = qb.match({
+            createdAt: { $lte: timeStr },
+            cid: { $lt: cid }
+        });
+    }
+
+    const res = await qb.exec();
+    const feed = res.map((row) => ({
+        post: row._id
+    }));
+
+    let cursor: string | undefined;
+    const last = res.at(-1);
+    if (last) {
+        cursor = `${new Date(last.createdAt).getTime()}::${last.cid}`;
+    }
+
+    return {
+        cursor,
+        feed
+    };
+};
--- feeder/algos/index.ts
+++ feeder/algos/index.ts
@@ -1,14 +1,15 @@
-import { AppContext } from '../index'
 import {
-  QueryParams,
-  OutputSchema as AlgoOutput,
-} from '../../common/lexicon/types/app/bsky/feed/getFeedSkeleton'
-import * as feedLangPt from './langPt'
+    QueryParams,
+    OutputSchema as AlgoOutput
+} from '../../common/lexicon/types/app/bsky/feed/getFeedSkeleton';
+import * as feedLangPt from './langPt';
+import * as feedBBB from './bbb';
 
-type AlgoHandler = (params: QueryParams) => Promise<AlgoOutput>
+type AlgoHandler = (params: QueryParams) => Promise<AlgoOutput>;
 
 const algos: Record<string, AlgoHandler> = {
-  [feedLangPt.shortname]: feedLangPt.handler,
-}
+    [feedLangPt.shortname]: feedLangPt.handler,
+    [feedBBB.shortname]: feedBBB.handler,
+};
 
-export default algos
+export default algos;
