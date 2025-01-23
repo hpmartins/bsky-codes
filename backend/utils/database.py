7293c064cffd0665f6852799ca7d04442ceb0db1
hpmartins <hpmartins@gmail.com>
2025-01-23 14:56:48 -0800

    added compression

--- backend/utils/database.py
+++ backend/utils/database.py
@@ -4,6 +4,7 @@ from pymongo.errors import ConnectionFailure
 
 from .core import Logger
 
+
 class MongoDBManager:
     def __init__(self, uri: str):
         self.uri = uri
@@ -13,8 +14,8 @@ class MongoDBManager:
     async def connect(self):
         """Connects to the MongoDB server."""
         try:
-            self.client = motor.motor_asyncio.AsyncIOMotorClient(self.uri)
-            await self.client.admin.command("ping") # test connection
+            self.client = motor.motor_asyncio.AsyncIOMotorClient(self.uri, compressors="zstd")
+            await self.client.admin.command("ping")
             self.logger.info(f"Connected to MongoDB at {self.uri}")
         except ConnectionFailure as e:
             self.logger.error(f"Failed to connect to MongoDB: {e}")
