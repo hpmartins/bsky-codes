604fd68eba771704de91f183c34faeda486a7d2a
hpmartins <hpmartins@gmail.com>
2025-02-06 02:16:47 -0800

    cleaning up

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -18,16 +18,5 @@ module.exports = {
       interpreter: "./.venv/bin/python",
       time: true,
     },
-    // {
-    //   name: "FART",
-    //   script: "./backend/FART.py",
-    //   watch: ["utils", "./backend/FART.py"],
-    //   args: [],
-    //   instances: "1",
-    //   wait_ready: true,
-    //   autorestart: true,
-    //   restart_delay: 500,
-    //   interpreter: "./.venv/bin/python",
-    // },
   ],
 };
